541c53f61893993cf9799a16df9d368872f0e078
Ragesh <ezhillragesh@gmail.com>
2023-12-04 08:38:27 +0530

    Time: 5 ms (19.13%) | Memory: 6.8 MB (91.88%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -5,7 +5,9 @@ public:
         for(int i=1;i<num.size()-1;++i){
             string temp="";
             if(num[i-1]==num[i] && num[i]==num[i+1]){
-               temp = num.substr(i - 1, 3);
+               temp = num[i - 1];
+               temp += num[i];
+               temp += num[i + 1];
             }
             if(temp>res) res=temp;
         }
