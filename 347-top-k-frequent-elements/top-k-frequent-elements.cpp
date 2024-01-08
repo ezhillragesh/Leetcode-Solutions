7c9071726f3c7b6c296d879db57d2a3644a790b2
Ragesh <ezhillragesh@gmail.com>
2024-01-08 19:54:23 +0530

    Time: 21 ms (15.17%) | Memory: 13.9 MB (90.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,33 +1,22 @@
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> umap;
+        for(auto i: nums) umap[i]++;
+        int cnt=0,ans=0;
         vector <int> res;
-        map <int,int> countMap;
-        for(int i=0; i<nums.size(); i++){
-            countMap[nums[i]]++;
-        }
-
-        int max,ans=0,cnt=0;
-        while(cnt<k){
-            max=0;
-            for(auto i :countMap){
-                if(i.second > max){
-                    max=i.second;
-                    ans=i.first;
+        while(k>cnt){
+            int max=INT_MIN;
+            for(auto num:umap){
+                if(num.second>max){
+                    max=num.second;
+                    ans=num.first;
                 }
             }
             res.push_back(ans);
-            countMap.erase(ans);
             cnt++;
+            umap.erase(ans);
         }
         return res;
-        
-        
-            
-        
-
     }
-
-    
 };
