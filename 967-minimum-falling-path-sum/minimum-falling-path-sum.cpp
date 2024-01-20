9e098b2ca5f02c08cea2cba9cbb07ff125086f31
Ragesh <ezhillragesh@gmail.com>
2024-01-20 11:02:43 +0530

    Time: 4 ms (99.54%) | Memory: 12.6 MB (5.08%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,12 +3,12 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         for(int i=matrix.size()-2;i>=0;i--){
             for(int j=0;j<matrix[i].size();j++){
-                if(i+1 < matrix.size()){
+               
                     int mini=matrix[i+1][j];
                     if(j>0) mini=min(mini,matrix[i+1][j-1]);
                     if(j < matrix[i].size()-1) mini=min(mini,matrix[i+1][j+1]);
                     matrix[i][j]+=mini;
-                }
+                
 
             }
         }
