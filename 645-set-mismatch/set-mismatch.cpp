d6752917d3f398924cf1c44ad5ab73c629063fa9
Ragesh <ezhillragesh@gmail.com>
2024-01-22 11:53:40 +0530

    Time: 46 ms (36.48%) | Memory: 34.5 MB (8.31%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -6,15 +6,15 @@ public:
         int aSum=0;
         int dup=0;
         int miss=0;
-        vector <int> result;
+        
         unordered_map<int,int> umap;
         
         for(auto it:nums){
-            cout<<it<<" ";
+           
             aSum+=it;
             umap[it]++;
         }
-        cout<<"actual sum is : " <<aSum << "expected is : "<<exSum<<" ";
+       
 
         for(auto it:umap){
             if(it.second == 2){
@@ -25,9 +25,8 @@ public:
        miss = exSum - (aSum - dup);
 
 
-        result.push_back(dup);
-        result.push_back(miss);
+       
 
-        return result;
+        return {dup,miss};
     }
 };
