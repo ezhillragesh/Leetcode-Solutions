1bbd0405327e3503b1c047eede618b5aba0f8cae
Ragesh <ezhillragesh@gmail.com>
2024-02-06 19:45:40 +0530

    Time: 80 ms (79.49%) | Memory: 96.4 MB (13.43%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
@@ -3,9 +3,9 @@ public:
     int findNonMinOrMax(vector<int>& nums) {
         if(nums.size()<=2) return -1;
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size()-1;i++){
-            return nums[i];
-        }
+       if(nums[1]) return nums[1];
+            
+        
         return -1;
     }
 };
